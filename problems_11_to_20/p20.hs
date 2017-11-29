61aff1e3a58fd23415470da8f4430abc32f3850d
Ejaaz Merali <ejaaz.merali@gmail.com>
2017-11-28 22:18:43 -0500

    Refactor removeAt

--- problems_11_to_20/p20.hs
+++ problems_11_to_20/p20.hs
@@ -1,7 +1,5 @@
 removeAt :: Int -> [a] -> (a, [a])
-removeAt n l = (l !! (n-1), removeAt' n l)
-
-removeAt' :: Int -> [a] -> [a]
-removeAt' _ [] = []
-removeAt' 1 (h:t) = t
-removeAt' n (h:t) = h : (removeAt' (n-1) t)
+removeAt 1 (h:t) = (h, t)
+removeAt n (h:t) =
+    let (removed, restOfList) = removeAt (n-1) t
+    in (removed, h:restOfList)
